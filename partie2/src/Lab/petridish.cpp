05ada7ced536d099a146fe5ab44f0dc6f9725703
Mapanao Justin Christian <justin.mapanao@epfl.ch>
2020-03-15 14:15:06 +0100

    Created the prototypes for the class petridish To do: Definitions

--- partie2/src/Lab/petridish.cpp
+++ partie2/src/Lab/petridish.cpp
@@ -1,6 +1,24 @@
 #include "petridish.hpp"
 
-Petridish::Petridish()
+Petridish::Petridish(Vec2d position,double radius)
 {
-
+ //TO DO.
 }
+
+bool Petridish::addBacterium(Bacterium*){
+   //TO DO.
+}
+bool Petridish::addNutrient(Nutriment*){
+    //TO DO.
+}
+bool Petridish::update(sf::Time dt){
+    //TO DO.
+}
+void Petridish::drawOn(sf::RenderTarget& targetWindow){
+    //TO DO.
+}
+void Petridish::reset(){
+    //TO DO.
+}
+
+
--- partie2/src/Lab/petridish.hpp
+++ partie2/src/Lab/petridish.hpp
@@ -1,11 +1,18 @@
-#ifndef PETRIDISH_HPP
-#define PETRIDISH_HPP
+#prgama once
+#include "bacterium.hpp"
+#include "Utility/Vec2d.hpp"
+#include <SFML/Graphics.hpp>
 
-
-class Petridish
+class Petridish:public CircularBody
 {
+    Bacterium bacterium;
+    Nutrient nutrient;
 public:
-    Petridish();
+    Petridish(Vec2d position,double radius);
+    bool addBacterium(Bacterium*);
+    bool addNutrient(Nutriment*);
+    void update(sf::Time dt);
+    void drawOn(sf::RenderTarget& targetWindow);
+    void reset();
 };
 
-#endif // PETRIDISH_HPP
