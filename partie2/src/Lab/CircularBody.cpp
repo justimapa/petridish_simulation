94282ad93494b9dab92fc0cfe3f30866e2df2c06
Germeyan Umut Tan <germeyan@INTRANET.EPFL.CH@IC-IN-SC-27.intranet.epfl.ch>
2020-03-19 22:26:56 +0100

    Removed unnecessary inclusions

--- partie2/src/Lab/CircularBody.cpp
+++ partie2/src/Lab/CircularBody.cpp
@@ -1,20 +1,8 @@
-#include "CircularBody.hpp"
 #include <iostream>
-#include "Utility/Vec2d.hpp"
+#include "CircularBody.hpp"
 
 using namespace std;
 
-CircularBody::CircularBody(Vec2d const& position,double const& radius):
-    position(position),
-    radius(radius){
-    //Done.
-}
-CircularBody::CircularBody(CircularBody const& copy):
-   position(copy.getPosition()),
-   radius(copy.getRadius())
-{
-    //Done.
-}
 Vec2d CircularBody::getPosition()const
 {
     return position;
@@ -23,30 +11,7 @@ double  CircularBody::getRadius()const
 {
     return radius;
 }
-void CircularBody::setPosition(Vec2d& position_)
-{
-    position=position_;
-}
-void CircularBody::setRadius(double radius_)
-{
-    radius=radius_;
-}
-bool CircularBody::contains(CircularBody const& other)const
-{
-    return(radius>=other.radius and distance(position,other.position)<=radius-other.radius);
-}
-bool CircularBody::isColliding(CircularBody const& other)const
-{
-    return distance(position,other.position)<=radius+other.radius;
-}
-bool CircularBody::contains(Vec2d const& point)const
-{
-    return distance(position,point)<=radius;
-}
-void CircularBody::move(Vec2d distance)
-{
-    position+=distance;
-}
+
 CircularBody& CircularBody::operator=(CircularBody& other)
 {
     position=other.position;
@@ -57,15 +22,53 @@ bool CircularBody::operator>(CircularBody const& body)
 {
     return contains(body);
 }
-
 bool CircularBody::operator&(CircularBody const& body)
 {
     return isColliding(body);
 }
-
 bool CircularBody::operator>(Vec2d const& point)
 {
     return contains(point);
+}
+
+void CircularBody::move(Vec2d distance)
+{
+    position+=distance;
+}
+bool CircularBody::contains(CircularBody const& other)const
+{
+    return(radius>=other.radius and distance(position,other.position)<=radius-other.radius);
+}
+bool CircularBody::contains(Vec2d const& point)const
+{
+    return distance(position,point)<=radius;
+}
+bool CircularBody::isColliding(CircularBody const& other)const
+{
+    return distance(position,other.position)<=radius+other.radius;
+}
+ostream& CircularBody::print(ostream& out) const
+{
+    out << "CircularBody: position = " << position << " radius = " << radius;
+    return out;
+}
+
+CircularBody::CircularBody(Vec2d const& position,double const& radius)
+: position(position),
+  radius(radius)
+{ }
+CircularBody::CircularBody(CircularBody const& copy)
+: position(copy.getPosition()),
+  radius(copy.getRadius())
+{ }
+
+void CircularBody::setPosition(Vec2d& position_)
+{
+    position=position_;
+}
+void CircularBody::setRadius(double radius_)
+{
+    radius=radius_;
 }
 
 ostream& operator<<(ostream& out, CircularBody const& body)
@@ -73,9 +76,6 @@ ostream& operator<<(ostream& out, CircularBody const& body)
     return body.print(out);
 }
 
-ostream& CircularBody::print(ostream& out) const
-{
-    out << "CircularBody: position = " << position << " radius = " << radius;
-    return out;
-}
+
+
 
