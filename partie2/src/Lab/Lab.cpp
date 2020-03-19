3ca5cb88683a094acf3a69488958ec4becead1c5
Germeyan Umut Tan <germeyan@INTRANET.EPFL.CH@IC-IN-SC-27.intranet.epfl.ch>
2020-03-19 22:29:54 +0100

    Removed unnescessary inclusions

--- partie2/src/Lab/Lab.cpp
+++ partie2/src/Lab/Lab.cpp
@@ -1,40 +1,43 @@
+#include "Application.hpp"
 #include "Lab.hpp"
-#include "petridish.hpp"
-#include "Nutriment.hpp"
-#include <Application.hpp>
 
 Lab::Lab()
     : dish(getApp().getCentre(),getApp().getLabSize().x*(0.95/2),
            getAppConfig()["petri dish"]["temperature"]["default"].toDouble())
+{ }
+
+double Lab::getTemperature() const
 {
-  //Done.
-}
-double Lab::getTemperature()const{
     return dish.getTemperature();
 }
-void Lab::decreaseTemperature(){
+void Lab::decreaseTemperature()
+{
     dish.decreaseTemperature();
 }
-void Lab::increaseTemperature(){
+void Lab::increaseTemperature()
+{
     dish.increaseTemperature();
 }
-void Lab::addNutriment(Nutriment* nutriment){
+void Lab::addNutriment(Nutriment* nutriment)
+{
     dish.addNutriment(nutriment);
 }
-void Lab::drawOn(sf::RenderTarget& targetWindow)const{
+void Lab::drawOn(sf::RenderTarget& targetWindow) const
+{
     dish.drawOn(targetWindow);
 }
-bool Lab::update(sf::Time dt){
+bool Lab::update(sf::Time dt)
+{
     dish.update(dt);
 }
-bool Lab::contains(const CircularBody& body){
-    if(dish>body)return true;
-    return false;
+bool Lab::contains(const CircularBody& body)
+{
+    return (dish > body);
 }
 void Lab::reset(){
     dish.reset();
 }
-
-Lab::~Lab(){
+Lab::~Lab()
+{
     reset();
 }
