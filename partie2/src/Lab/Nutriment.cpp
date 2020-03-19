3960cd8e82a9b13995c217996c74580d9a42a648
Germeyan Umut Tan <germeyan@INTRANET.EPFL.CH@IC-IN-SC-27.intranet.epfl.ch>
2020-03-19 22:31:23 +0100

    Removed unnecessary inclusion and modularized update function

--- partie2/src/Lab/Nutriment.cpp
+++ partie2/src/Lab/Nutriment.cpp
@@ -1,24 +1,16 @@
 #include "Nutriment.hpp"
-#include <SFML/Graphics.hpp>
-#include "CircularBody.hpp"
-#include <Utility/Types.hpp>
-#include "Utility/Vec2d.hpp"
+#include <string>
 #include "Application.hpp"
 #include "petridish.hpp"
-#include <string>
-using namespace std;
 
+using namespace std;
 
 Nutriment::Nutriment(Quantity const& quantity,Vec2d const& position)
 : CircularBody(position, quantity),
   quantity(quantity)
+{ }
+Quantity Nutriment::takeQuantity(Quantity quantity_)
 {
-    //Done.
-}
-
-
-
-Quantity Nutriment::takeQuantity(Quantity quantity_){
     Quantity quantity_taken(quantity_);
     if(quantity >= quantity_){
         quantity -= quantity_;
@@ -31,16 +23,16 @@ Quantity Nutriment::takeQuantity(Quantity quantity_){
         return quantity_taken;
     }
 }
-
-void Nutriment::setQuantity(Quantity quantity_){
+void Nutriment::setQuantity(Quantity quantity_)
+{
     if(quantity_ >= 0.0){
         quantity = quantity_;
     } else {
         quantity = 0.0;
     }
 }
-
-void Nutriment::drawOn(sf::RenderTarget& target) const{
+void Nutriment::drawOn(sf::RenderTarget& target) const
+{
     auto const& texture = getAppTexture(getConfig()["texture"].toString());
     auto nutrimentSprite = buildSprite(getPosition(),getRadius(), texture);
     nutrimentSprite.setScale(2 * getRadius() / texture.getSize().x, 2 * getRadius() /
@@ -52,20 +44,28 @@ void Nutriment::drawOn(sf::RenderTarget& target) const{
         target.draw(text);
     }
 }
-
-j::Value const& Nutriment::getConfig() const{
+j::Value const& Nutriment::getConfig() const
+{
     return getAppConfig()["nutriments"];
 }
-
-void Nutriment::update(sf::Time dt){
-    if(getAppEnv().getTemperature() >= getConfig()["growth"]["min temperature"].toDouble()
-       and getAppEnv().getTemperature() <= getConfig()["growth"]["max temperature"].toDouble()
-       and quantity <= 2 * getConfig()["quantity"]["max"].toDouble()
-       and getAppEnv().contains(*this)){
+void Nutriment::update(sf::Time dt)
+{
+    if(isTemperatureOK() and isQuantityOK() and isContained()){
         auto growth = getConfig()["growth"]["speed"].toDouble() * dt.asSeconds();
         quantity+=growth;
         setRadius(quantity);
-
     }
 }
-
+bool Nutriment::isTemperatureOK() const
+{
+    return getAppEnv().getTemperature() >= getConfig()["growth"]["min temperature"].toDouble()
+            and getAppEnv().getTemperature() <= getConfig()["growth"]["max temperature"].toDouble();
+}
+bool Nutriment::isQuantityOK() const
+{
+    return quantity <= 2 * getConfig()["quantity"]["max"].toDouble();
+}
+bool Nutriment::isContained() const
+{
+    return getAppEnv().contains(*this);
+}
