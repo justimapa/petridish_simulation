342b97c4f1eff4a96a093132a9a982d9c065b76c
Germeyan Umut Tan <germeyan@INTRANET.EPFL.CH@IC-IN-SC-27.intranet.epfl.ch>
2020-03-19 22:30:28 +0100

    Added comments

--- partie2/src/Lab/Lab.hpp
+++ partie2/src/Lab/Lab.hpp
@@ -1,6 +1,7 @@
 #pragma once
-#include "petridish.hpp"
 #include "Nutriment.hpp"
+#include "petridish.hpp"
+
 
 class Lab
 {
@@ -9,13 +10,45 @@ public:
      * @brief Default constructor
      */
     Lab();
-    double getTemperature()const;
+    /*!
+     * @brief Getter for the Petridish::'s temperature
+     * @return Temperature of the Petridish::
+     */
+    double getTemperature() const;
+    /*!
+     * @brief Decreases temperature
+     */
     void decreaseTemperature();
+    /*!
+     * @brief Increases temperature
+     */
     void increaseTemperature();
+    /*!
+     * @brief Tests if a CircularBody:: is inside another
+     * @param The other CircularBody::
+     * @return true if the other CircularBody:: has a smaller radius and the center is
+     * within our CircularBody::, false if not
+     */
     bool contains(const CircularBody&);
+    /*!
+     * @brief Adds a Nutriment:: to the Petridish::
+     * @param Nutriment:: to be added
+     */
     void addNutriment(Nutriment*);
-    void drawOn(sf::RenderTarget& targetWindow)const;
+    /*!
+     * @brief Draws Petridish:: on targetWindow
+     * @param targetWindow
+     */
+    void drawOn(sf::RenderTarget& targetWindow) const;
+    /*!
+     * @brief Updates Lab:: after every fraction of time dt
+     * @param Fraction of time dt after which the Lab:: is updates
+     * @return true if update is a success, false if not
+     */
     bool update(sf::Time dt);
+    /*!
+     * @brief Destructs Lab::
+     */
     void reset();
     ~Lab();
 
