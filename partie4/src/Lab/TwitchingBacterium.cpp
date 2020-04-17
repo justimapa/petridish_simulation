28396ba5e2640d3f180470f12aefc9a334cebd23
Germeyan Umut Tan <germeyan@INTRANET.EPFL.CH@IC-IN-SC-15.intranet.epfl.ch>
2020-04-17 10:41:45 +0200

    Set up class Twitching Bacterium

---
+++ partie4/src/Lab/TwitchingBacterium.cpp
@@ -0,0 +1,6 @@
+#include "TwitchingBacterium.hpp"
+
+TwitchingBacterium::TwitchingBacterium()
+{
+
+}
---
+++ partie4/src/Lab/TwitchingBacterium.hpp
@@ -0,0 +1,11 @@
+#ifndef TWITCHINGBACTERIUM_HPP
+#define TWITCHINGBACTERIUM_HPP
+
+
+class TwitchingBacterium
+{
+public:
+    TwitchingBacterium();
+};
+
+#endif // TWITCHINGBACTERIUM_HPP
