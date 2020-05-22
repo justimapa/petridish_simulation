ab338f8ccb65847ca8e9f89d465ae653b37f6b8a
Mapanao Justin Christian <mapanao@INTRANET.EPFL.CH@IC-IN-SC-29.intranet.epfl.ch>
2020-05-22 16:19:53 +0200

    Added comments

--- partie6/src/Stats/Stats.hpp
+++ partie6/src/Stats/Stats.hpp
@@ -9,19 +9,56 @@ class Stats:public Drawable,public Updatable
 {
 public:
     Stats();
-    void drawOn(sf::RenderTarget&) const;
-    void update(sf::Time dt);
-    std::string getCurrentTitle();
+    /*!
+     * @brief Draws the Stats::
+     * @param targetWindow where the Stats:: will be drawn
+     */
+    virtual void drawOn(sf::RenderTarget&) const override;
+    /*!
+     * @brief Updates stats at every fraction
+     * of time dt
+     * @param Fraction of time dt
+     */
+    virtual void update(sf::Time dt) override;
+    /*!
+     * @brief Getter for the Title of the current Graph::
+     * @return A string of the title
+     */
+    std::string getCurrentTitle() const;
+    /*!
+     * @brief Setter for the active Graph::
+     */
     void setActive(int);
+    /*!
+     * @brief Increments the active graph Id
+     */
     void next();
+    /*!
+     * @brief Decrements the active graph Id
+     */
     void previous();
+    /*!
+     * @brief Reset all the graphs
+     */
     void reset();
+    /*!
+     * @brief Adds a Graph:: to the map of Graphs
+     * @param Integer Id of the Graphs
+     * @param String Title of the Graphs
+     * @param names series' titles
+     * @param min    y-axis: min value expected
+     * @param max    y-axis: max value expected
+     * @param size   size on screen
+     */
     void addGraph(int id,std::string title,std::vector<std::string> const& names,double min,double max,Vec2d const& size);
     /*!
      * @brief Resets generator's stopwatch to 0
      */
     void resetStopwatch();
-    ~Stats();
+    /*!
+     * @brief Destructor for Stats
+     */
+    virtual ~Stats() override;
 private:
     int currentid;
     sf::Time stopwatch;
