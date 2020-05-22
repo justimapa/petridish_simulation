26797a9b670e59ac625c2c40ba33a539d6f91023
Germeyan Umut Tan <germeyan@INTRANET.EPFL.CH@IC-IN-SC-74.intranet.epfl.ch>
2020-05-22 08:33:06 +0200

    Did a few comments

--- partie6/src/Lab/Bacteriophage.hpp
+++ partie6/src/Lab/Bacteriophage.hpp
@@ -4,13 +4,43 @@
 #include "Interface/Updatable.hpp"
 #include "Utility/DiffEqSolver.hpp"
 
+/*!
+ * @class Bacterium
+ * @brief class that represents the functions and behavior
+ * of bacteriophage within a Petridish
+ *
+ * Subclass of CircularBody, Drawable and Updatable
+ */
 class Bacteriophage : public CircularBody, public Drawable, public Updatable, public DiffEqFunction
 {
 public:
+    /*!
+     * @brief Constructs a Bacteriophage:: fully set up
+     * @param Position
+     * @param Radius
+     * @param Radius
+     * @return A Bacteriophage:: with all parameters set
+     */
     Bacteriophage(Vec2d const& position, double const& radius, Vec2d const& direction);
+    /*!
+     * @brief Draws the Bacteriophage::
+     * @param targetWindow where the Bacteriophage:: will be drawn
+     */
     virtual void drawOn(sf::RenderTarget &target) const override;
+    /*!
+     * @brief Updates phages within Petridish:: at every fraction
+     * of time dt
+     * @param Fraction of time dt
+     */
     virtual void update(sf::Time dt) override;
+    /*!
+     * @brief Moves the Bacteriophage:: within the Petridish::
+     * @param Time dt after which movement will take place
+     */
     void move(sf::Time dt);
+    /*!
+     * @brief Changes the direction of tumble depending
+     */
     void aim(sf::Time dt);
     void infect() const;
     Vec2d getDirection() const;
